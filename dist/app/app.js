5c451a9fe31957d52d79d39530d88077ba5994ee
= <=>
2024-05-22 02:48:41 +0600

    Final commit

---
+++ dist/app/app.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const products_route_1 = require("./modules/products/products.route");
+const orders_route_1 = require("./modules/orders/orders.route");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+// Routes will go here
+app.use("/api", products_route_1.ProductsRoute, orders_route_1.OrderRoutes);
+// Handling unavilable routes
+app.all("*", (req, res) => {
+    res.status(404).json({
+        status: false,
+        message: "Route not found",
+    });
+});
+exports.default = app;
